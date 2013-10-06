00c6f6e1afd3356d3b29014a89e6db58dddff904
aagiza <afdal.software>
2013-10-06 12:43:42 +0200

    initial commit

---
+++ JavaProject/src/Student.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author aagiza
+ * This is just a test
+ */
+public class Student {
+
+	/**
+	 * 
+	 */
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
